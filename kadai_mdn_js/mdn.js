cb8008b3f28440c74b618b67375f89ae0550108d
makizaemon <tainobu1226@gmail.com>
2025-12-10 13:08:40 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,5 +1,6 @@
 // 今日の日付を取得
-const today = new Date(2024, 9, 12); // 月は0始まりなので、10月は9
+const today = new Date(); // 月は0始まりなので、10月は9
 
 // コンソールに出力
+
 console.log(`${today.getFullYear()}年${today.getMonth() + 1}月${today.getDate()}日`);
